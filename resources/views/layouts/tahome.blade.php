931afa7d430d1fb4aa930a3db96bd39245f328e1
3A732080 <tiffany89031176@gmail.com>
2021-02-03 13:12:55 +0800

    修正連結

--- resources/views/layouts/tahome.blade.php
+++ resources/views/layouts/tahome.blade.php
@@ -58,7 +58,7 @@
     <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('students.home')}}">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('students.index')}}">
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-sticky-note"></i>
             </div>
